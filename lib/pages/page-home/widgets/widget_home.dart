8242ada69df98456757ce96c9570fc3e417b5f9c
Ronny Rusmayadi <ronirusmayadi4@gmail.com>
2021-02-24 11:58:17 +0700

    add qiblah

--- lib/pages/page-home/widgets/widget_home.dart
+++ lib/pages/page-home/widgets/widget_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/services.dart';
-import 'package:sahabatqu/pages/page_qiblah.dart';
+import 'package:sahabatqu/pages/page-qiblah/page_qiblah.dart';
 import 'package:sahabatqu/pages/page_quran.dart';
 
 import '../../../constants/themes-color.dart';
---
+++ lib/pages/page-qiblah/page_qiblah.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
+import 'package:sahabatqu/pages/page-qiblah/loading_indicator.dart';
+import 'package:sahabatqu/pages/page-qiblah/qiblah_compas_widget.dart';
+import 'package:sahabatqu/pages/page-qiblah/qiblah_maps.dart';
+
+import '../../constants/themes-color.dart';
+
+class QiblahPage extends StatefulWidget {
+  @override
+  _QiblahPageState createState() => _QiblahPageState();
+}
+
+class _QiblahPageState extends State<QiblahPage> {
+  final _deviceSupport = FlutterQiblah.androidDeviceSensorSupport();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          iconTheme: IconThemeData(color: ColorPalette.themeColor),
+          backgroundColor: Colors.white,
+          title: Text(
+            "Kiblat",
+            style: TextStyle(
+                color: ColorPalette.themeColor, fontWeight: FontWeight.bold),
+          )),
+      body: FutureBuilder(
+        future: _deviceSupport,
+        builder: (_, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return LoadingIndicator();
+          if (snapshot.hasError)
+            return Center(
+              child: Text("Error: ${snapshot.error.toString()}"),
+            );
+
+          if (snapshot.data)
+            return QiblahCompass();
+          else
+            return QiblahMaps();
+        },
+      ),
+    );
+  }
+}
