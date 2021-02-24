9ae906746a0a14cd5996ed1134b1fc7316a8125c
Ronny Rusmayadi <ronirusmayadi4@gmail.com>
2021-02-24 19:26:39 +0700

    add quran page

---
+++ lib/models/quran_model.dart
@@ -0,0 +1,114 @@
+// To parse this JSON data, do
+//
+//     final quranModel = quranModelFromJson(jsonString);
+
+import 'dart:convert';
+
+QuranModel quranModelFromJson(String str) =>
+    QuranModel.fromJson(json.decode(str));
+
+String quranModelToJson(QuranModel data) => json.encode(data.toJson());
+
+class QuranModel {
+  QuranModel({
+    this.status,
+    this.query,
+    this.hasil,
+  });
+
+  String status;
+  Query query;
+  List<Hasil> hasil;
+
+  factory QuranModel.fromJson(Map<String, dynamic> json) => QuranModel(
+        status: json["status"] == null ? null : json["status"],
+        query: json["query"] == null ? null : Query.fromJson(json["query"]),
+        hasil: json["hasil"] == null
+            ? null
+            : List<Hasil>.from(json["hasil"].map((x) => Hasil.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status == null ? null : status,
+        "query": query == null ? null : query.toJson(),
+        "hasil": hasil == null
+            ? null
+            : List<dynamic>.from(hasil.map((x) => x.toJson())),
+      };
+}
+
+class Hasil {
+  Hasil({
+    this.nomor,
+    this.nama,
+    this.asma,
+    this.name,
+    this.start,
+    this.ayat,
+    this.type,
+    this.urut,
+    this.rukuk,
+    this.arti,
+    this.keterangan,
+  });
+
+  String nomor;
+  String nama;
+  String asma;
+  String name;
+  String start;
+  String ayat;
+  String type;
+  String urut;
+  String rukuk;
+  String arti;
+  String keterangan;
+
+  factory Hasil.fromJson(Map<String, dynamic> json) => Hasil(
+        nomor: json["nomor"] == null ? null : json["nomor"],
+        nama: json["nama"] == null ? null : json["nama"],
+        asma: json["asma"] == null ? null : json["asma"],
+        name: json["name"] == null ? null : json["name"],
+        start: json["start"] == null ? null : json["start"],
+        ayat: json["ayat"] == null ? null : json["ayat"],
+        type: json["type"] == null ? null : json["type"],
+        urut: json["urut"] == null ? null : json["urut"],
+        rukuk: json["rukuk"] == null ? null : json["rukuk"],
+        arti: json["arti"] == null ? null : json["arti"],
+        keterangan: json["keterangan"] == null ? null : json["keterangan"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "nomor": nomor == null ? null : nomor,
+        "nama": nama == null ? null : nama,
+        "asma": asma == null ? null : asma,
+        "name": name == null ? null : name,
+        "start": start == null ? null : start,
+        "ayat": ayat == null ? null : ayat,
+        "type": type == null ? null : type,
+        "urut": urut == null ? null : urut,
+        "rukuk": rukuk == null ? null : rukuk,
+        "arti": arti == null ? null : arti,
+        "keterangan": keterangan == null ? null : keterangan,
+      };
+}
+
+class Query {
+  Query({
+    this.format,
+    this.surat,
+  });
+
+  String format;
+  String surat;
+
+  factory Query.fromJson(Map<String, dynamic> json) => Query(
+        format: json["format"] == null ? null : json["format"],
+        surat: json["surat"] == null ? null : json["surat"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "format": format == null ? null : format,
+        "surat": surat == null ? null : surat,
+      };
+}
---
+++ lib/pages/page-quran/page_quran.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:sahabatqu/models/quran_model.dart';
+import 'package:sahabatqu/pages/page-quran/page_detail_quran.dart';
+import 'package:sahabatqu/viewmodel/quran_vm.dart';
+
+import '../../constants/themes-color.dart';
+
+class QuranPage extends StatefulWidget {
+  @override
+  _QuranPageState createState() => _QuranPageState();
+}
+
+class _QuranPageState extends State<QuranPage> {
+  @override
+  Widget build(BuildContext context) {
+    QuranViewModel quranVM = QuranViewModel();
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          backgroundColor: ColorPalette.themeColor,
+          elevation: 0,
+          title: Text(
+            "Al-Quran",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+      body: FutureBuilder<QuranModel>(
+        future: quranVM.getDataQuran(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final QuranModel quranModel = snapshot.data;
+            return ListView.separated(
+              itemCount: quranModel.hasil.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return DetailQuranPage(
+                          idNumber: quranModel.hasil[index].nomor,
+                          name: quranModel.hasil[index].nama,
+                          type: quranModel.hasil[index].type);
+                    }));
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Text(
+                                (index + 1).toString() + ".",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    quranModel.hasil[index].nama,
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        color: ColorPalette.themeColor,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  SizedBox(
+                                    height: 2,
+                                  ),
+                                  Text(
+                                    quranModel.hasil[index].arti +
+                                        " (" +
+                                        quranModel.hasil[index].ayat +
+                                        ")",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey[600],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          quranModel.hasil[index].asma,
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  thickness: 1,
+                );
+              },
+            );
+          }
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(
+                  height: 8,
+                ),
+                Text("Mohon Tunggu...")
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
